9b3952655747c85e13d8ddb37759caf609c618e6
JHTNT <jerry1106666@gmail.com>
2023-11-04 01:40:11 +0800

    feat: probe function

--- board_solver.h
+++ board_solver.h
@@ -60,4 +60,42 @@ UpdateSet propagate(Board &G, const Clues &d, Status &status) {
     return PI;
 }
 
+UpdateSet probe(Board &G, const Clues &d, char row, char col, Status &status) {
+    Status status_gp0 = Status::INCOMPLETE;
+    Status status_gp1 = Status::INCOMPLETE;
+    vector<string> G_p0(G);
+    vector<string> G_p1(G);
+
+    G_p0[row][col] = '0';
+    UpdateSet pi_gp0 = propagate(G_p0, d, status_gp0);
+    G_p1[row][col] = '1';
+    UpdateSet pi_gp1 = propagate(G_p1, d, status_gp1);
+
+    if (status_gp0 == Status::CONFLICT && status_gp1 == Status::CONFLICT) {
+        status = Status::CONFLICT;
+        return UpdateSet();
+    }
+
+    UpdateSet pi;
+    Board *src = &G_p0;
+    if (status_gp0 == Status::CONFLICT)
+        pi = pi_gp0;
+    else if (status_gp1 == Status::CONFLICT)
+        pi = pi_gp1, src = &G_p1;
+    else
+        set_intersection(pi_gp0.begin(), pi_gp0.end(), pi_gp1.begin(), pi_gp1.end(),
+                         inserter(pi, pi.begin()));
+
+    if (!pi.empty()) {
+        for (short p : pi) {
+            char r = p / 100, c = p % 100;
+            G[r][c + 1] = (*src)[r][c + 1];
+        }
+        status = Status::PAINTED;
+    } else {
+        status = Status::INCOMPLETE;
+    }
+    return pi;
+}
+
 #endif
