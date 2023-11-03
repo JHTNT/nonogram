a0ab087f56a0b7c859196074f260b3a6ba1c19c4
JHTNT <jerry1106666@gmail.com>
2023-11-04 01:41:51 +0800

    feat: output result of `fp1()`

--- nonogram.cpp
+++ nonogram.cpp
@@ -1,22 +1,39 @@
 #include <bits/stdc++.h>
 
+#include "board_solver.h"
 #include "config.h"
 #include "file_io.h"
-#include "line_solve.h"
 
 int main() {
     in.open("data/one_testcase.in");
     out.open("data/one_testcase.out");
 
     string testcase;
-    vector<vector<short>> clues(SIZE * 2);
-    vector<vector<char>> board(SIZE, vector<char>(SIZE, 'u'));
 
     while (!in.fail() && in >> testcase) {
+        vector<vector<short>> clues(SIZE * 2);
+        Board board(SIZE * 2, string(SIZE + 1, 'u'));
+        for (int i = 0; i < SIZE * 2; i++) board[i][0] = i;
+
+        Status status = INCOMPLETE;
         out << testcase << "\n";
         get_testcase(clues);
+
+        clock_t s, e;
+        s = clock();
+        fp1(board, clues, status);
+        e = clock();
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 1; j <= SIZE; j++) {
+                out << board[i][j] << ' ';
+            }
+            out << '\n';
+        }
+        out << "Time used: " << (double) (e - s) / CLOCKS_PER_SEC << " s\n";
+
+        in.close();
+        out.close();
+        return 0;
     }
-    in.close();
-    out.close();
-    return 0;
 }
